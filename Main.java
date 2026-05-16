d6df51f13c197dcc3a81f9faf1cb04f60f075530
samia55-cell <sm.samia555@gmail.com>
2026-05-16 21:58:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        FileManager fileManagement = new FileManagement();
+        FileManagement fileManagement = new FileManagement();
 
         List<Student> students = new ArrayList<>();
 
